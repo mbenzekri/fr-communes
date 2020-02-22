8a9c8ec41da0ecd3ba1b13fb443edd3b50804082
Mohamed <mbenzekri@users.noreply.github.com>
2020-02-22 14:38:02 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import {OSM, TileDebug} from 'ol/source';
+
+
+var map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    }),
+    new TileLayer({
+      source: new TileDebug()
+    })
+  ],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 1
+  })
+});
