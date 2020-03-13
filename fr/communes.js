42ef307d5ad626c55da93ea867d65c0c0917863c
mbenzekri <benzekri.mohamed@gmail.com>
2020-03-14 00:32:32 +0100

    correction for code insee length

--- fr/communes.js
+++ fr/communes.js
@@ -1,5 +1,5 @@
 function pixel2key(pixel) {
     const hcolor = ((pixel[0] * 256) + pixel[1]) * 256 + pixel[2]
-    const key = parseInt(hcolor.toString(16), 10).toString().padStart(6, '0')
+    const key = parseInt(hcolor.toString(16), 10).toString().padStart(5, '0')
     return key
 }
